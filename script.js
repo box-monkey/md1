6e1faef81d5c359b8dcce4d472c3920983522bda
box-monkey <jr.gasparin15@gmail.com>
2022-05-08 03:17:53 -0400

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+let mothersDayText = document.getElementById("mothers-day");
+let mothersDayText2 = document.getElementById("mothers-day2");
+const myBtn = document.getElementById("btn");
+
+myBtn.addEventListener("click", function () {
+  document.getElementById("mothers-day").textContent =
+    "Happy Mothers Day To The Most Amazing Mom In The Whole Wide World! Loving, Compassionate, Selfless, Supportive And Funny. Short List Of What We Love About You!";
+  document.getElementById("mothers-day2").textContent =
+    "Love - Samuel, Yoda, Brin and Dad.";
+});
